d150ed0ff5dd345920cbae16235d1f6c28546cf1
David <davidsaleh03@gmail.com>
2026-02-19 20:48:38 -0800

    new

--- app/dashboard/for-you/page.tsx
+++ app/dashboard/for-you/page.tsx
@@ -1,30 +1,19 @@
 'use client'
-import { useGetRecommendedBooksQuery, useGetSelectedBookQuery } from '@/redux/reccomenedSlice'
+import Selected from '@/components/Selected'
 import Link from 'next/link'
 import React from 'react'
 
 const ForYou: React.FC = () => {
-const { data, isLoading, isError } = useGetRecommendedBooksQuery()
   return (
+    <div className="wrapper">
     <div className="row">
         <div className="container">
             <div className="for-you__wrapper">
                 <div className="for-you__title">Selected just for you</div>
-                <Link href='/book' className='selected__book'>
-                <div className="selected__book--sub-title">How Constant Innovation Creates Radically Successful Businesses</div>
-                <div className="selected__book--line"></div>
-                <div className="selected__book--content">
-                    <figure className="book__image--wrapper">
-
-                    </figure>
-                    <div className="selected__book--text">
-                        <div className="selected__book--title">The Lean Startup</div>
-
-                    </div>
-                </div>
-                </Link>
+                <Selected />
             </div>
         </div>
+    </div>
     </div>
   )
 }
--- components/Selected.tsx
+++ components/Selected.tsx
@@ -1,19 +1,38 @@
 import Link from "next/link";
 import React from "react";
 import { useGetSelectedBookQuery } from "@/redux/reccomenedSlice";
+import Image from "next/image";
+import { FaPlay } from "react-icons/fa";
+import { IoPlayCircle } from "react-icons/io5";
+import { IoIosPlay } from "react-icons/io";
 
 const Selected = () => {
-    const { data, isLoading, isError } = useGetSelectedBookQuery()
+    const { data } = useGetSelectedBookQuery()
+
+   
+   const selectedBook = data?.[0]
   return (
     <Link href="/book" className="selected__book">
       <div className="selected__book--sub-title">
-        How Constant Innovation Creates Radically Successful Businesses
+         {selectedBook?.subTitle}
       </div>
       <div className="selected__book--line"></div>
       <div className="selected__book--content">
-        <figure className="book__image--wrapper"></figure>
+        <figure className="book__image--wrapper image--wrapper--2">
+            {selectedBook?.imageLink && (
+                <img className="book__image"src={selectedBook.imageLink} alt='book-page'/>
+            )
+        }
+        </figure>
         <div className="selected__book--text">
-          <div className="selected__book--title">The Lean Startup</div>
+          <div className="selected__book--title">{selectedBook?.title}</div>
+          <div className="selected__book--author">{selectedBook?.author}</div>
+          <div className="selected__book--duration-wrapper">
+            <div className="selected__book--icon">
+                <IoIosPlay />
+            </div>
+            <div className="selected__book--duration">3 mins 23 secs</div>
+          </div>
         </div>
       </div>
     </Link>
