547b23d3438e96ddf46af2f8513f070781c3f3a1
David <davidsaleh03@gmail.com>
2026-02-23 01:03:28 -0800

    working on login

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react'
 import { FaGoogle, FaUser } from 'react-icons/fa'
 import { HiXMark } from 'react-icons/hi2'
@@ -8,25 +9,49 @@ import { register, login } from "@/redux/firebase/authService";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/redux/store";
 import { closeModal } from "@/redux/modalSlice";
+import { ImSpinner2 } from 'react-icons/im'
 
 const Login = () => {
+    const [loading, setLoading] = useState(true)
     const dispatch = useDispatch();
     const modal = useSelector((state: RootState) => state.modal.isOpen);  
     const handleGoogleSignIn = async () => {
     try {
+        setLoading(false);
       const user = await signInWithGoogle();
       console.log("Logged in user:", user);
+      setLoading(true);
       dispatch(closeModal()); 
     } catch (err) {
-      console.error("Google Sign-In Error:", err);
-    }
+    console.error(err);
+    setLoading(false);
+}
   };
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     try {
+        setLoading(false);
       await register(email, password);
+      setLoading(true);
+      dispatch(closeModal());
     }
-    catch{}
+    catch (err) {
+    console.error(err);
+    setLoading(false);
+}
+  };
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+        setLoading(false);
+      await login(email, password);
+      setLoading(true);
+      dispatch(closeModal());
+    }
+    catch (err) {
+    console.error(err);
+    setLoading(false);
+}
   };
   
     const [isLogin, setIsLogin] = useState(true)
@@ -51,7 +76,15 @@ const Login = () => {
                         <figure className="google__icon--mask guest__icon--mask">
                             <FaUser />
                         </figure>
-                        <div>Login as a Guest</div>
+                        {
+                            loading 
+                            ?
+                            <div>Login as a Guest</div>
+                            :
+                            <div className="spinner__icon--wrapper">
+                                <ImSpinner2 />
+                            </div>
+                        }
                     </button>
                     <div className="auth__separator">
                         <span className="auth__separator--text">or</span>
@@ -60,17 +93,37 @@ const Login = () => {
                         <figure className="google__icon--mask">
                             <Image src={google} alt="logo" />
                         </figure>
-                        <div >Login with Google</div>
-                    </button>
+                        {
+                            loading
+                            ?
+                            <div >Login with Google</div>
+                            :
+                            <div className="spinner__icon--wrapper">
+                                <ImSpinner2 />
+                            </div>
+                        }
+                        </button>
                     <div className="auth__separator">
                         <span className="auth__separator--text">or</span>
                     </div>
-                    <form className="auth__main--form" onSubmit={() => login(email, password)}>
+                    <form className="auth__main--form" onSubmit={handleLogin}>
                         <input type="email" className="auth__main--input" placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} required/>
                         <input type="password" className="auth__main--input" placeholder='Password' onChange={(e) => setPassword(e.target.value)} required/>
+                        {
+                            loading
+                            ?
                         <button className="btn">
                             <span>Login</span>
                         </button>
+                        :
+                        <button className="btn" disabled>
+                            <div className="spinner__icon--wrapper">
+                                <ImSpinner2 />
+                            </div>
+                        </button>
+
+
+                        }
                     </form>
                 </div>
                 <div className="auth__forgot--password">Forgot your password?</div>
@@ -81,9 +134,17 @@ const Login = () => {
 
                 <div className="auth__content">
                     <div className="auth__title">Sign up to Summarist</div>
-                    <button className="btn google__btn--wrapper">
+                    <button className="btn google__btn--wrapper" onClick={handleGoogleSignIn}>
                         <figure className="google__icon--mask"><FaGoogle /></figure>
-                        <div>Login with Google</div>
+                        {
+                            loading
+                            ?
+                            <div >Sign up with Google</div>
+                            :
+                            <div className="spinner__icon--wrapper">
+                                <ImSpinner2 />
+                            </div>
+                        }
                     </button>
                     <div className="auth__separator">
                         <span className="auth__separator--text">or</span>
@@ -91,9 +152,21 @@ const Login = () => {
                     <form className="auth__main--form" onSubmit={handleSubmit}>
                         <input type="email" className="auth__main--input" placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} required/>
                         <input type="password" className="auth__main--input" placeholder='Password' onChange={(e) => setPassword(e.target.value)} required/>
+                        {
+                            loading
+                            ?
                         <button className="btn">
                             <span>Sign up</span>
                         </button>
+                        :
+                        <button className="btn" disabled>
+                            <div className="spinner__icon--wrapper">
+                                <ImSpinner2 />
+                            </div>
+                        </button>
+
+
+                        }
                     </form>
                 </div>
                 <button className="auth__switch--btn" onClick={() => setIsLogin(true)}>Already have an account?</button>
